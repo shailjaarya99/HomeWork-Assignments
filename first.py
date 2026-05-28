5feb63ebc6446292162247375be0d9807b605bd0
shailjaarya99 <229571141+shailjaarya99@users.noreply.github.com>
2026-05-28 16:15:33 +0530

    Add files via upload
    learning git with progress in terminals

---
+++ first.py
@@ -0,0 +1,5 @@
+cost = 39.9
+item = "pajama"
+print("The cost of the " + item + " is " + str(cost) )
+
+
---
+++ second.py
@@ -0,0 +1,4 @@
+def say_hello(name):
+    return f"hello, {name}"
+message = say_hello("shail")
+print(message)
