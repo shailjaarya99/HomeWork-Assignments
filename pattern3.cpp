6abaf932b5eda4952379f0d314cab4e078f006bc
shailjaarya99 <229571141+shailjaarya99@users.noreply.github.com>
2026-07-18 16:38:51 +0530

    Add output example as comment in pattern3.cpp
    The code was modified to include a comment block that demonstrates the output for a 6-column pattern. The functionality remains unchanged.

--- pattern3.cpp
+++ pattern3.cpp
@@ -1,4 +1,11 @@
-//to print number from a input till b input, just that for every number at col-th the table continues in new row from the next number
+/*/*enter no. of col. :6
+1 2 3 4 5 6 
+7 8 9 10 11 12 
+13 14 15 16 17 18 
+19 20 21 22 23 24 
+25 26 27 28 29 30 
+31 32 33 34 35 36 
+*/
 #include <iostream>
 #include <string>
 using namespace std;
